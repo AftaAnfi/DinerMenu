94cff6666222cb45410da71a54dbc2f2ec5749dc
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-02-21 11:21:04 -0700

    Added Buttons and Label

--- DinerMenu/DinerMenuForm.Designer.vb
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -22,10 +22,90 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 415)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(75, 23)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(93, 415)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(75, 23)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(174, 415)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(75, 23)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(255, 415)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Location = New System.Drawing.Point(332, 21)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(39, 13)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Label1"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.AutoSize = True
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(332, 63)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(39, 13)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.Text = "Label2"
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
 End Class
--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -1,3 +1,13 @@
-﻿Public Class DinerMenuForm
+﻿Option Strict On
+Option Explicit On
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Diner Menu Program
+'https://github.com/AftaAnfi/DinerMenu.git
+
+Public Class DinerMenuForm
+
+
 
 End Class
